b7d13d89c890cd10d700fb44a976e6fd2a1f4475
simhacker <simhacker@d9718cc8-9f43-0410-858b-315f434eb58c>
2008-12-23 19:57:29 +0000

    Applied 46_usersoundon_bool.patch

--- MicropolisCore/src/MicropolisEngine/fileio.cpp
+++ MicropolisCore/src/MicropolisEngine/fileio.cpp
@@ -237,7 +237,7 @@ int Micropolis::loadFile(
   autoBulldoze = (MiscHis[52] != 0);   // flag for autoBulldoze
   autoBudget   = (MiscHis[53] != 0);   // flag for autoBudget
   autoGo       = (MiscHis[54] != 0);   // flag for auto-goto
-  UserSoundOn = MiscHis[55];    /* flag for the sound on/off */
+  UserSoundOn  = (MiscHis[55] != 0);   // flag for the sound on/off
   CityTax = MiscHis[56];
   SimSpeed = MiscHis[57];
   ChangeCensus();
@@ -331,7 +331,7 @@ int Micropolis::saveFile(
   MiscHis[52] = autoBulldoze;   // flag for autoBulldoze
   MiscHis[53] = autoBudget;     // flag for autoBudget
   MiscHis[54] = autoGo;         // flag for auto-goto
-  MiscHis[55] = UserSoundOn;    /* flag for the sound on/off */
+  MiscHis[55] = UserSoundOn;    // flag for the sound on/off
   MiscHis[57] = SimSpeed;
   MiscHis[56] = CityTax;        /* post release */
 
