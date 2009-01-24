e27677eb3998b794ed2b65ed4484b96db9c2cdb3
alberth289346 <alberth289346@d9718cc8-9f43-0410-858b-315f434eb58c>
2009-01-24 08:57:35 +0000

    Shuffled the TileEngine sources + build to the pyMicropolis structure

--- MicropolisCore/src/CellEngine/setup.py
+++ MicropolisCore/src/CellEngine/setup.py
@@ -1,43 +1,43 @@
-# celldrawingarea.py
+# setup.py
 #
 # Micropolis, Unix Version.  This game was released for the Unix platform
 # in or about 1990 and has been modified for inclusion in the One Laptop
 # Per Child program.  Copyright (C) 1989 - 2007 Electronic Arts Inc.  If
 # you need assistance with this program, you may contact:
 #   http://wiki.laptop.org/go/Micropolis  or email  micropolis@laptop.org.
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or (at
 # your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful, but
 # WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 # General Public License for more details.  You should have received a
 # copy of the GNU General Public License along with this program.  If
 # not, see <http://www.gnu.org/licenses/>.
-# 
+#
 #             ADDITIONAL TERMS per GNU GPL Section 7
-# 
+#
 # No trademark or publicity rights are granted.  This license does NOT
 # give you any right, title or interest in the trademark SimCity or any
 # other Electronic Arts trademark.  You may not distribute any
 # modification of this program using the trademark SimCity or claim any
 # affliation or association with Electronic Arts Inc. or its employees.
-# 
+#
 # Any propagation or conveyance of this program must include this
 # copyright notice and these terms.
-# 
+#
 # If you convey this program (or any modifications of it) and assume
 # contractual liability for the program to recipients of it, you agree
 # to indemnify Electronic Arts for any liability that those contractual
 # assumptions impose on Electronic Arts.
-# 
+#
 # You may not misrepresent the origins of this program; modified
 # versions of the program must be marked as such and not identified as
 # the original program.
-# 
+#
 # This disclaimer supplements the one included in the General Public
 # License.  TO THE FULLEST EXTENT PERMISSIBLE UNDER APPLICABLE LAW, THIS
 # PROGRAM IS PROVIDED TO YOU "AS IS," WITH ALL FAULTS, WITHOUT WARRANTY
@@ -61,96 +61,31 @@
 
 
 ########################################################################
-# Cell Drawing Area
+# CellEngine setup file.
 # Don Hopkins
 
 
-########################################################################
-# Import stuff
+from distutils.core import *
 
-
-import sys
-import os
-import random
-import array
-
-
-########################################################################
-# Import our modules
-
-
-import cellpiemenus
-from tiledrawingarea import TileDrawingArea
-import celltool
-from tiletool import TileTool
-
-
-########################################################################
-
-
-class CellDrawingArea(TileDrawingArea):
-
-
-    def __init__(
-        self,
-        engine=None,
-        **args):
-
-        args['sourceTileSize'] = 16
-        args['worldRows'] = 100
-        args['worldCols'] = 100
-        args['tileCount'] = 960
-
-        self.engine = engine
-
-        TileDrawingArea.__init__(self, **args)
-
-
-    def configTileEngine(self, tengine):
-
-        engine = self.engine
-        tengine.setBuffer(engine.GetCellBuffer())
-        tengine.width = engine.width
-        tengine.height = engine.height
-        tengine.colBytes = 1
-        tengine.rowBytes = engine.width
-        tengine.typeCode = 'B'
-        tengine.tileMask = 0xff
-
-
-    def makeTileMap(
-        self):
-
-        tileMap = array.array('i')
-        self.tileMap = tileMap
-        for i in range(0, self.tileCount):
-            tileMap.append(random.randint(0, 959))
-
-
-    def destroyEngine(self):
-
-        TileDrawingArea.destroyEngine(self)
-
-
-    def getCell(self, col, row):
-
-        return self.engine.GetCell(col, row)
-
-
-    def makeToolPie(self):
-
-        toolPie = cellpiemenus.MakeToolPie(lambda toolName: self.selectToolByName(toolName))
-        self.toolPie = toolPie
-
-
-    def handleButtonPress(
-        self,
-        widget,
-        event):
-
-        self.handleToolPieButtonPress(
-            widget,
-            event)
+setup(
+    name='CellEngineInterface',
+    version='1.0',
+    description='Cellular Automata Machine Engine Interface',
+    author='Don Hopkins',
+    author_email='dhopkins@DonHopkins.com',
+    url='http://www.DonHopkins.com',
+    ext_modules=[
+        Extension('_cellengine', [
+            'objs/cellengine_wrap.cpp',
+            'src/cellengine.cpp',
+            'src/ruletables.cpp',
+        ],
+        include_dirs=[
+            'src',
+            '/usr/local/include',
+        ]),
+  ]
+)
 
 
 ########################################################################
