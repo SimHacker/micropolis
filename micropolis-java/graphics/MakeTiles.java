3b86e45e506d409576b065fb8f1346035bb6bda8
jason@long.name <jason@long.name@d9718cc8-9f43-0410-858b-315f434eb58c>
2013-06-26 13:12:52 +0000

    graphics: MakeTiles: support layering two or more images together

--- micropolis-java/graphics/MakeTiles.java
+++ micropolis-java/graphics/MakeTiles.java
@@ -42,31 +42,42 @@ public class MakeTiles
 			String tileImage = tileData.get(Integer.toString(i));
 			assert tileImage != null;
 
-			ImageRef ref = parseImageRef(tileImage);
-			if (!loadedImages.containsKey(ref.fileName)) {
-				loadedImages.put(ref.fileName,
-					loadImage(ref.fileName));
-			}
-
-			Image sourceImg = loadedImages.get(ref.fileName);
-			int srcWidth = sourceImg.getWidth(null);
-			int srcHeight = sourceImg.getHeight(null);
-
-			gr.drawImage(
-				sourceImg,
-				0, TILE_SIZE*i,
-				TILE_SIZE, TILE_SIZE*(i+1),
-				ref.offsetX, ref.offsetY,
-				ref.offsetX + (ref.width != 0 ? ref.width : srcWidth),
-				ref.offsetY + (ref.height != 0 ? ref.height : srcHeight),
-				null);
+			ImageSpec ref = parseImageSpec(tileImage);
+			drawTo(ref, gr, 0, TILE_SIZE*i);
 		}
 
 		ImageIO.write(buf, "png", outputFile);
 	}
 
-	static class ImageRef
+	static void drawTo(ImageSpec ref, Graphics2D gr, int destX, int destY)
+		throws IOException
 	{
+		if (ref.background != null) {
+			drawTo(ref.background, gr, destX, destY);
+		}
+
+		if (!loadedImages.containsKey(ref.fileName)) {
+			loadedImages.put(ref.fileName,
+				loadImage(ref.fileName));
+		}
+
+		Image sourceImg = loadedImages.get(ref.fileName);
+		int srcWidth = sourceImg.getWidth(null);
+		int srcHeight = sourceImg.getHeight(null);
+
+		gr.drawImage(
+			sourceImg,
+			destX, destY,
+			destX+TILE_SIZE, destY+TILE_SIZE,
+			ref.offsetX, ref.offsetY,
+			ref.offsetX + (ref.width != 0 ? ref.width : srcWidth),
+			ref.offsetY + (ref.height != 0 ? ref.height : srcHeight),
+			null);
+	}
+
+	static class ImageSpec
+	{
+		ImageSpec background;
 		String fileName;
 		int offsetX;
 		int offsetY;
@@ -74,31 +85,39 @@ public class MakeTiles
 		int height;
 	}
 
-	static ImageRef parseImageRef(String s)
+	static ImageSpec parseImageSpec(String chain)
 	{
-		ImageRef rv = new ImageRef();
+		ImageSpec result = null;
 
-		String [] parts = s.split("@", 2);
+		for (String layerStr : chain.split("\\|")) {
+
+		ImageSpec rv = new ImageSpec();
+		rv.background = result;
+		result = rv;
+
+		String [] parts = layerStr.split("@", 2);
 		rv.fileName = parts[0];
 
 		if (parts.length >= 2) {
-		String offsetInfo = parts[1];
-		parts = offsetInfo.split(",");
-		if (parts.length >= 1) {
-			rv.offsetX = Integer.parseInt(parts[0]);
-		}
-		if (parts.length >= 2) {
-			rv.offsetY = Integer.parseInt(parts[1]);
-		}
-		if (parts.length >= 3) {
-			rv.width = Integer.parseInt(parts[2]);
-		}
-		if (parts.length >= 4) {
-			rv.height = Integer.parseInt(parts[3]);
-		}
+			String offsetInfo = parts[1];
+			parts = offsetInfo.split(",");
+			if (parts.length >= 1) {
+				rv.offsetX = Integer.parseInt(parts[0]);
+			}
+			if (parts.length >= 2) {
+				rv.offsetY = Integer.parseInt(parts[1]);
+			}
+			if (parts.length >= 3) {
+				rv.width = Integer.parseInt(parts[2]);
+			}
+			if (parts.length >= 4) {
+				rv.height = Integer.parseInt(parts[3]);
+			}
 		}//endif something given after '@' in image specifier
 
-		return rv;
+		}//end foreach layer in image specification
+
+		return result;
 	}
 
 	static Image loadImage(String fileName)
