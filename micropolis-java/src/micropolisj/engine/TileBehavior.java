544d206af4c404da5dec8d3e6d5a2b313c02fba4
jason@long.name <jason@long.name@d9718cc8-9f43-0410-858b-315f434eb58c>
2013-10-04 12:53:52 +0000

    behaviors: define a new class TileBehavior to be a base class
    this will be a base class of all tile-behavior classes

---
+++ micropolis-java/src/micropolisj/engine/TileBehavior.java
@@ -0,0 +1,39 @@
+// This file is part of MicropolisJ.
+// Copyright (C) 2013 Jason Long
+// Portions Copyright (C) 1989-2007 Electronic Arts Inc.
+//
+// MicropolisJ is free software; you can redistribute it and/or modify
+// it under the terms of the GNU GPLv3, with additional terms.
+// See the README file, included in this distribution, for details.
+
+package micropolisj.engine;
+
+import java.util.Random;
+import static micropolisj.engine.TileConstants.*;
+
+public abstract class TileBehavior
+{
+	protected final Micropolis city;
+	protected final Random PRNG;
+	int xpos;
+	int ypos;
+	int tile;
+	int rawTile;
+
+	protected TileBehavior(Micropolis city)
+	{
+		this.city = city;
+		this.PRNG = city.PRNG;
+	}
+
+	public final void processTile(int xpos, int ypos)
+	{
+		this.xpos = xpos;
+		this.ypos = ypos;
+		this.rawTile = city.getTile(xpos, ypos);
+		this.tile = rawTile & LOMASK;
+		apply();
+	}
+
+	public abstract void apply();
+}
